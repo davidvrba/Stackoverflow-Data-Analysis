2a41a79178c8c297a8d6ce49acd74b8261d4ca2b
vrbad <david.vrba@emplifi.io>
2021-12-03 13:35:08 +0100

    installation changes

--- conf/installation.sh
+++ conf/installation.sh
@@ -7,8 +7,7 @@ pip3 install boto3
 pip3 install requests
 apt-get install p7zip-full
 mkdir data
-cd /home/ubuntu/
-python3 Stackoverflow-Data-Analysis/src/data_download.py 
+python3 Stackoverflow-Data-Analysis/src/data_download.py
 cd data
-7z x /home/ubuntu/data/data.7z
+7z x /home/ubuntu/data/posts.7z
 jupyter notebook --nobrowser --port=8888
